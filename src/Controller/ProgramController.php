c35de15f3b01ecbd65e0136302166f8f401d2b1f
stephanie meyer <stephanie2510@gmail.com>
2023-12-04 11:56:38 +0100

    fin quete 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
@@ -16,7 +17,7 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/program/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'program_show')]
+    #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'show')]
     public function show(int $id): Response
     {
         return $this->render('program/show.html.twig', [
