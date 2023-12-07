6a7fb4d457016ca03ca253a8994467aed2ca8508
stephanie meyer <stephanie2510@gmail.com>
2023-12-07 23:06:45 +0100

    quete 10 en cours-

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Controller;
+
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,12 +10,27 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
 {
-    #[Route('/category/', name: 'category_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        $category = $categoryRepository->findAll();
+    $category = $categoryRepository->findAll();
 
-        return $this->render('category/index.html.twig', [
+    return $this->render('category/index.html.twig', [
+        'categories' => $categoryName,
+    ]);
+    }
+
+    #[Route('/{categoryName}', methods: ['GET'], name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findBy();
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category found in category table'
+            );
+        }
+        return $this->render('category/show.html.twig', [
             'category' => $category,
         ]);
     }
